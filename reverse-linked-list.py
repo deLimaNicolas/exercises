36ab8e175cefd2888fa249964c63b799b49c9c9e
deLimaNicolas <teixlima@gmail.com>
2025-06-04 11:43:49 -0300

    more exc

--- reverse-linked-list.py
+++ reverse-linked-list.py
@@ -15,3 +15,15 @@ class Solution:
             curr = tmp
 
         return prev
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        
+        new_head = self.reverseList(head.next)
+        
+        head.next.next = head
+        head.next = None
+        
+        return new_head
