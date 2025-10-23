7e128d614345fcc110260f1ea0a119127d2e5e8a
deLimaNicolas <teixlima@gmail.com>
2025-10-23 20:34:18 -0300

    reset

--- concatenation-of-array.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def getConcatenation(self, nums: List[int]) -> List[int]:
-        LEN = len(nums)
-        ans = [0] * LEN * 2    
-        for i in range(LEN):
-            ans[i] = nums[i]
-            ans[i + LEN] = nums[i]
-
-        return ans
