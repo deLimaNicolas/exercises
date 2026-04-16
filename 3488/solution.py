757fa28a846d4d6188febf88568606b6d4ce8f2f
deLimaNicolas <teixlima@gmail.com>
2026-04-16 17:57:18 -0300

    3488

--- 3488/solution.py
+++ 3488/solution.py
@@ -3,11 +3,23 @@ class Solution:
         n = len(nums)
         elm_idxs = defaultdict(list)
 
+        def find_pos(arr, elm):
+            l, r = 0, len(arr) - 1
+
+            while l <= r:
+                mid = l + (r - l) // 2
+                if arr[mid] == elm:
+                    return mid
+                if arr[mid] > elm:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+
         def circular_dist(a, b):
             return min(abs(a - b), n - abs(a - b))
 
         def find_nearest(curr_idxs, q):
-            i = bisect_left(curr_idxs, q)
+            i = find_pos(curr_idxs, q)
             left = circular_dist(curr_idxs[(i - 1) % len(curr_idxs)], q)
             right = circular_dist(curr_idxs[(i + 1) % len(curr_idxs)], q)
             return min(left, right)
